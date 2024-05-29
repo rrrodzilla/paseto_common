af273738e2a650563f71b618c4bf7f6b6b6d86ca
Roland Rodriguez <roland@govcraft.ai>
2024-05-29 12:10:50 -0600

    refactor: remove feature flags and related conditional code

--- src/lib.rs
+++ src/lib.rs
@@ -8,21 +8,13 @@ pub mod prelude {
 
     pub use crate::error::PasetoKeyError;
     pub use crate::keys::Key;
-    #[cfg(feature = "local")]
     pub use crate::purpose::Local;
-    #[cfg(feature = "public")]
     pub use crate::purpose::Public;
-    #[cfg(any(feature = "v1", feature = "v3"))]
     pub use crate::traits::Nist;
-    #[cfg(any(feature = "v2", feature = "v4"))]
     pub use crate::traits::Sodium;
     pub use crate::traits::{ImplicitAssertionCapable, PurposeTrait, VersionTrait};
-    #[cfg(feature = "v1")]
     pub use crate::version::V1;
-    #[cfg(feature = "v2")]
     pub use crate::version::V2;
-    #[cfg(feature = "v3")]
     pub use crate::version::V3;
-    #[cfg(feature = "v4")]
     pub use crate::version::V4;
 }
--- src/version/mod.rs
+++ src/version/mod.rs
@@ -1,17 +1,9 @@
-#[cfg(any(feature = "v1", doc))]
 mod v1;
-#[cfg(any(feature = "v2", doc))]
 mod v2;
-#[cfg(any(feature = "v3", doc))]
 mod v3;
-#[cfg(any(feature = "v4", doc))]
 mod v4;
 
-#[cfg(any(feature = "v1", doc))]
 pub use v1::V1;
-#[cfg(any(feature = "v2", doc))]
 pub use v2::V2;
-#[cfg(any(feature = "v3", doc))]
 pub use v3::V3;
-#[cfg(any(feature = "v4", doc))]
 pub use v4::V4;
