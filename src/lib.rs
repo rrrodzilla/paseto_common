67a261d1ce8861472a8f1efdcfe5de1e5ae3c006
Roland Rodriguez <roland@govcraft.ai>
2024-05-29 13:22:40 -0600

    refactor(lib): restructure module exports
    Commented out error and keys modules in lib.rs.
    Restructured prelude module to include submodules for purposes, traits, and versions.
    Added a newline in traits.rs for better readability.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,26 @@
-mod error;
-mod keys;
+// mod error;
+// mod keys;
 mod purpose;
 mod traits;
 mod version;
 
 pub mod prelude {
 
-    pub use crate::error::PasetoKeyError;
-    pub use crate::keys::Key;
-    pub use crate::purpose::Local;
-    pub use crate::purpose::Public;
-    pub use crate::traits::Nist;
-    pub use crate::traits::Sodium;
-    pub use crate::traits::{ImplicitAssertionCapable, PurposeTrait, VersionTrait};
-    pub use crate::version::V1;
-    pub use crate::version::V2;
-    pub use crate::version::V3;
-    pub use crate::version::V4;
+    // pub use crate::error::PasetoKeyError;
+    // pub use crate::keys::Key;
+    pub mod purposes{
+        pub use crate::purpose::Local;
+        pub use crate::purpose::Public;
+    }
+    pub mod traits {
+        pub use crate::traits::Nist;
+        pub use crate::traits::Sodium;
+        pub use crate::traits::{ImplicitAssertionCapable, PurposeTrait, VersionTrait};
+    }
+    pub mod versions{
+        pub use crate::version::V1;
+        pub use crate::version::V2;
+        pub use crate::version::V3;
+        pub use crate::version::V4;
+    }
 }
--- src/traits.rs
+++ src/traits.rs
@@ -1,5 +1,6 @@
 use std::fmt::Display;
 
+
 //marker traits
 /// Used by marker traits to determine at compile time which PASETO version the user is attempting to use
 pub trait VersionTrait: Display + Default + AsRef<str> {}
